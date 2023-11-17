3faf5982634b4fdca8e05c81197443ee8debf3bb
Eric Carlsson <97894605+eric-carlsson@users.noreply.github.com>
2023-11-17 17:48:00 +0100

    Disable tmux by default

--- home/.config/bash/custom.sh
+++ home/.config/bash/custom.sh
@@ -14,8 +14,3 @@ if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
     GIT_PROMPT_ONLY_IN_REPO=1
     source "$HOME/.bash-git-prompt/gitprompt.sh"
 fi
-
-# run tmux by default if it exists
-if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
-  exec tmux
-fi
