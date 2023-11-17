d38f7f78b0064139b21d7004c31e3d5019671a55
Eric Carlsson <97894605+eric-carlsson@users.noreply.github.com>
2023-11-17 16:31:36 +0100

    Source fzf keybinds

--- .config/bash/custom.sh
+++ .config/bash/custom.sh
@@ -6,6 +6,9 @@ alias k="kubectl"
 source <(kubectl completion bash)
 complete -o default -F __start_kubectl k
 
+# fzf setup
+source /usr/share/doc/fzf/examples/key-bindings.bash
+
 # run tmux by default if it exists
 if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
   exec tmux
